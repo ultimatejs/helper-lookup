e72d1111dbfe86ae007b3fae5dc32fb87b5a85e1
James Gillmore <james@faceyspacey.com>
2015-12-09 21:15:17 -0800

    depend on ultimatejs:ultimate-react

--- package.js
+++ package.js
@@ -1,45 +1,22 @@
 Package.describe({
 	name: "ultimatejs:helper-lookup",
 	summary: "Blaze style method lookup & more",
-	version: '0.0.2',
+	version: '0.0.3',
 	documentation: 'README.md',
 	git: 'https://github.com/ultimatejs/helper-lookup'
 });
 
 Package.onUse(function (api) {
-	api.versionsFrom('METEOR@1.2.1');
+	api.use('ultimatejs:ultimate-react@0.0.0');
 
-	api.use('ecmascript@0.1.5');
-	
-	api.use('underscore');
-	
+	api.use('underscore@1.0.0');
+
 	api.addFiles([
-		'helper-lookup-mixin.js',
-		'global-helper.js',
+		'helper-lookup-mixin.jsx',
+		'global-helper.jsx',
 	]);
 
 	api.export([
 		'HelperLookup', 
 	]);
-	
-  api.use([
-    'babel-runtime@0.1.4-rc.0',
-    'react-runtime@0.14.1_1'
-  ]);
-	
-  api.imply([
-    'babel-runtime@0.1.4-rc.0',
-    'react-runtime@0.14.1_1'
-  ]);
-	
-  api.use('isobuild:compiler-plugin@1.0.0');
-});
-
-
-Package.registerBuildPlugin({
-  name: 'compile-jsx',
-  use: ['babel-compiler@5.8.22-rc.1'],
-  sources: [
-    'jsx-plugin.js'
-  ]
 });
